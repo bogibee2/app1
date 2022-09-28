e546ed0cda89168b7f9676e356ebde68b9334c4c
bogibee2 <bhageerath.bogi2@shipt.com>
2022-09-28 00:00:56 -0500

    web app complete

--- app1/stream_lit.py
+++ app1/stream_lit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import json 
 import requests
 
-st.title("Basic Caculator App 3")
+st.title("Basic Caculator App")
 
 # taking user inpputs 
 option = st.selectbox ('What operation You want to perform?',
@@ -11,12 +11,14 @@ option = st.selectbox ('What operation You want to perform?',
 st.write("") 
 st.write("Select the numbers from slider below 🔥 ")
 
-x = st.slider("x", 0, 100, 20) y = st.slider("Y", 0, 130, 10)
+x = st.slider("x", 0, 100, 20) 
+y = st.slider("Y", 0, 130, 10)
 
-#converting the inputs into a json format inputs = {"operation": option, "x": x,
-"y": y}
-# when the user clicks on button it will fetch the API if st.button('Calculate'):
-res = requests.post(url = "http://127.0.0.1:8000/calculate", data = json.dumps(inputs))
-st.subheader(f"Response from API
-=
-{res.text}").
+#converting the inputs into a json format 
+inputs = {"operation": option, "x": x,"y": y}
+
+# when the user clicks on button it will fetch the API 
+if st.button('Calculate'):
+
+    res = requests.post(url = "http://127.0.0.1:5009/calculate", data = json.dumps(inputs))
+    st.subheader(f"Response from API = {res.text}")
